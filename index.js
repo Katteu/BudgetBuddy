6cb5d6e0add5c38b21daf1fd677ae8c5a97d9c1c
Katteu <katheamari@gmail.com>
2024-02-03 23:23:17 +0800

    Setup Changes

--- index.js
+++ index.js
@@ -3,14 +3,22 @@
  */
 
 import { AppRegistry } from 'react-native';
+import {useEffect} from 'react';
 import App from './App';
 import { name as appName } from './app.json';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen'
 
-const AppWrapper = () => (
-  <NavigationContainer>
-    <App />
-  </NavigationContainer>
-);
+const AppWrapper = () => {
+  useEffect(() => {
+    // Hide the splash screen after the component has mounted
+    SplashScreen.hide();
+  }, []);
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+}
 
 AppRegistry.registerComponent(appName, () => AppWrapper);
